ea5597537c1b07c24cb1ddffdaa181c916ba4dd4
Rachelle Ragasa <rachelleragasa.costa@gmail.com>
2024-01-16 18:33:04 +0000

    use gatsby template

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+// support for .env, .env.development, and .env.production
+require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.cirilofilipinokainan.co.uk/",
+    title: "Cirilo Filipino Kainan",
+    author: `Gatsby`,
+    description: "A family-owned eatery serving hearty Filipino food",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        downloadLocal: true,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-vanilla-extract",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Cirilo Filipino Kainan",
+        short_name: "Cirilo",
+        start_url: "/",
+        // These can be imported once ESM support lands
+        background_color: "#ffe491",
+        theme_color: "#004ca3",
+        icon: "src/favicon.png",
+      },
+    },
+  ],
+}
