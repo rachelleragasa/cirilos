5a960452707ad4d89613c8d2fbf1139ec9c5328f
Rachelle Ragasa <rachelleragasa.costa@gmail.com>
2026-03-27 11:46:26 +0000

    build: add github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/cirilos",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
