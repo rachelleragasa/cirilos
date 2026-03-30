59d2ada24e527b98a7c7d37fff8e32e90c5d56ad
Rachelle Ragasa <rachelleragasa.costa@gmail.com>
2026-03-30 11:26:17 +0100

    fix: fix config file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+    ? `${process.env.NEXT_PUBLIC_BASE_PATH}/`
+    : "",
   images: {
     unoptimized: true,
     qualities: [25, 50, 75],
